a7a5be4da91a67e68e61a7b056bc6eecddc2e616
beoneben10 <bestthan01@gmail.com>
2017-02-02 09:59:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,6 +21,12 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			
+			if($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
+				$sticker = $event['massage']['sticker'];
+				$replyToken = $event['replyToken'];
+				$massage = ['type' => 'sticker' , 'sticker' => $sticker];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
