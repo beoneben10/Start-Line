27151c7e5e193a916cd8ab378deb53a3fb212667
beoneben10 <bestthan01@gmail.com>
2017-02-02 15:08:11 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,18 +2,7 @@
 $post = file_get_contents('php://input');
 $urlReply = 'https://api.line.me/v2/bot/message/reply';
 $token = 'SmC479aBsZtxrVoqsqEW1KDMT6c/bP6woDbRg2BD56+k/NM86O4XKvG68KVShh4mImg9IQJbE+QaVdGlSUSAbhzuxYN60cRrWdrqa6eyTAf2aOZO2IxluB9A4tAFYjepYUVSX+R1OKfrl9JX2zlc3QdB04t89/1O/w1cDnyilFU=';
-function postMessage($token,$packet,$urlReply){
- $dataEncode = json_encode($packet);
- $headersOption = array(‘Content-Type: application/json’,’Authorization: Bearer ‘.$token);
- $ch = curl_init($urlReply);
- curl_setopt($ch,CURLOPT_CUSTOMREQUEST,’POST’);
- curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
- curl_setopt($ch,CURLOPT_POSTFIELDS,$dataEncode);
- curl_setopt($ch,CURLOPT_HTTPHEADER,$headersOption);
- curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
- $result = curl_exec($ch);
- curl_close($ch);
-}
+
 $res = json_decode($post, true);
 if(isset($res[‘events’]) && !is_null($res[‘events’])){
  foreach($res[‘events’] as $item){
@@ -49,3 +38,15 @@ break;
 }
 	 $packet = getSticker($item[‘replyToken’]);
  postMessage($token,$packet,$urlReply);
+function postMessage($token,$packet,$urlReply){
+ $dataEncode = json_encode($packet);
+ $headersOption = array(‘Content-Type: application/json’,’Authorization: Bearer ‘.$token);
+ $ch = curl_init($urlReply);
+ curl_setopt($ch,CURLOPT_CUSTOMREQUEST,’POST’);
+ curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+ curl_setopt($ch,CURLOPT_POSTFIELDS,$dataEncode);
+ curl_setopt($ch,CURLOPT_HTTPHEADER,$headersOption);
+ curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+}
