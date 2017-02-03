f364f56eb973c7256f44e62d616f1af304b705f5
beoneben10 <bestthan01@gmail.com>
2017-02-03 08:41:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,29 +17,6 @@ function getSticker($replyToken){
    return $packet;
 }
  
- $res = json_decode($post, true);
- if(isset($res['events']) && !is_null($res['events'])){
- foreach($res['events'] as $item){
- if($item['type'] == 'message'){
- switch($item['message']['type']){
- case 'text':
- break;
- case 'image':
- break;
- case 'video':
- 
- break;
- case 'audio':
- 
- break;
- case 'location':
- break;
- case 'sticker':
- $packet = getSticker($item[‘replyToken’]);
- postMessage($token,$packet,$urlReply);
- break;
- }
- 
  function postMessage($token,$packet,$urlReply){
  $dataEncode = json_encode($packet);
  $headersOption = array('Content-Type: application/json','Authorization: Bearer '.$token);
@@ -53,3 +30,29 @@ function getSticker($replyToken){
  curl_close($ch);
 }
 
+ $res = json_decode($post, true);
+ if(isset($res['events']) && !is_null($res['events'])){
+ foreach($res['events'] as $item){
+ if($item['type'] == 'message'){
+ switch($item['message']['type']){
+ case 'text':
+   
+ break;
+ case 'image':
+   
+ break;
+ case 'video':
+ 
+ break;
+ case 'audio':
+ 
+ break;
+ case 'location':
+   
+ break;
+ case 'sticker':
+ $packet = getSticker($item['replyToken']);
+ postMessage($token,$packet,$urlReply);
+ break;
+ }
+ 
