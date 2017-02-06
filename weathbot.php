3f080936e4582dbee79f709ef435a3725f4f60fd
beoneben10 <bestthan01@gmail.com>
2017-02-06 08:39:39 +0700

    Update weathbot.php

--- weathbot.php
+++ weathbot.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$access_token = 'CVm19KJhT/H3jqz63wtllGGTBtoL8NNO1z4N2LjXPjNXU9ZArY6SWg6zOLwbj+WWk9utfeMETNuou56fWtLP6wJcRjM+gcS36u+bSYsyuHOrus+CsXf/aqRBF5VSya3yvB8gzxORHxHgzuYUq2Ly7gdB04t89/1O/w1cDnyilFU=';
+$access_token = 'SmC479aBsZtxrVoqsqEW1KDMT6c/bP6woDbRg2BD56+k/NM86O4XKvG68KVShh4mImg9IQJbE+QaVdGlSUSAbhzuxYN60cRrWdrqa6eyTAf2aOZO2IxluB9A4tAFYjepYUVSX+R1OKfrl9JX2zlc3QdB04t89/1O/w1cDnyilFU=';
 require_once "Knowlege_Base.php";
 require_once "GetDataJSON.php";
 // Get POST body content
