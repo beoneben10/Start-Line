1b833ce3941a20e8383cbe3911c2e13cad641fbc
beoneben10 <bestthan01@gmail.com>
2017-02-08 16:03:32 +0700

    Update GetDataJSON.php

--- GetDataJSON.php
+++ GetDataJSON.php
@@ -8,7 +8,7 @@ class GetData
         public static function getIndexProvince($provinceTHName)
         {
 
-                $province = "แม่ฮ่องสอน แม่สะเรียง เชียงราย เชียงราย พะเยา ดอยอ่างขาง เชียงใหม่ ลำปาง เถิน ลำปาง ลำพูน แพร่ น่าน น่าน ท่าวังผา ทุ่งช้าง อุตรดิตถ์ หนองคาย เลย เลย อุดรธานี สกลนคร สกลนคร นครพนม นครพนม หนองบัวลำภู สุโขทัย ศรีสำโรง ตาก แม่สอด เขื่อนภูมิพล ดอยมูเซอร์ อุ้มผาง พิษณุโลก เพชรบูรณ์ หล่มสัก วิเชียรบุรี กำแพงเพชร ขอนแก่น ท่าพระ มุกดาหาร พิจิตร มหาสารคาม กาฬสินธุ์ นครสวรรค์ ตากฟ้า ชัยนาท ชัยภูมิ ร้อยเอ็ด ร้อยเอ็ด อุบลราชธานี อุบลราชธานี ศรีสะเกษ พระนครศรีอยุธยา ปทุมธานี ฉะเชิงเทรา ราชบุรี สุพรรณบุรี อู่ทอง ลพบุรี บัวชุม นำร่อง สมุทรปราการ สนามบินสุวรรณภูมิ ปราจีนบุรี กบินทร์บุรี นครราชสีมา ปากช่อง โชคชัย สุรินทร์ สุรินทร์ ท่าตูม บุรีรัมย์ นางรอง อรัญประเทศ สระแก้ว กาญจนบุรี ทองผาภูมิ นครปฐม กรุงเทพมหานคร กรุงเทพฯท่าเรือคลองเตย กรุงเทพฯบางนา สนามบินดอนเมือง ชลบุรี เกาะสีชัง พัทยา สัตหีบ เพชรบุรี ระยอง ห้วยโป่ง จันทบุรี พลิ้ว ประจวบคีรีขันธ์ หัวหิน หนองพลับ ตราด ชุมพร สวี ระนอง สุราษฎร์ธานี เกาะสมุย สุราษฎร์ธานี นครศรีธรรมราช นครศรีธรรมราช ฉวาง พัทลุง ตะกั่วป่า ภูเก็ต ภูเก็ต เกาะลันตา กระบี่ ตรัง คอหงษ์ สะเดา สงขลา หาดใหญ่ สตูล ปัตตานี ยะลา นราธิวาส";
+                $province = "สำนักงาน ห้องพัก";
                 $ex_province = explode(" ", $province);
 
                 foreach ($ex_province as $key => $value) {
@@ -17,13 +17,13 @@ class GetData
                         }
                 }
         }
-        public static function getWeatherToday($province)
+        public static function getDevision($province)
         {
                 header ('Content-type: text/html; charset=utf-8');
 
                 $ch = curl_init(); 
                 // set url สำหรับดึงข้อมูล 
-                curl_setopt($ch, CURLOPT_URL, "http://data.tmd.go.th/api/WeatherToday/V1/"); 
+                curl_setopt($ch, CURLOPT_URL, "https://www.namo.xyz/lineben/myfile.json"); 
                 //return the transfer as a string 
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
                 // ตัวแปร $output เก็บข้อมูลทั้งหมดที่ดึงมา 
@@ -40,7 +40,7 @@ class GetData
                         $data_temp_min = $obj->Stations[$index]->Observe->MinTemperature->Value;
                         $data_relativeHumidity = $obj->Stations[$index]->Observe->RelativeHumidity->Value;
                         $data_windSpeed = $obj->Stations[$index]->Observe->WindSpeed->Value;
-                        $data_rainfall = $obj->Stations[$index]->Observe->Rainfall->Value;
+                       
 
                         $data_array = array(
                         "province" => "{$data_province}",
@@ -49,7 +49,7 @@ class GetData
                         "temperature_min" => "{$data_temp_min}",
                         "relativeHumidity" => "{$data_relativeHumidity}",
                         "windSpeed" => "{$data_windSpeed}",
-                        "rainfall" => "{$data_rainfall}",
+                        
                         "KEY" => "{$index}",
                         );
 
