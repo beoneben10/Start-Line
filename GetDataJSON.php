ca54e4c2a10a6b6d967856afaa01bf82835b7dff
beoneben10 <bestthan01@gmail.com>
2017-02-09 16:39:48 +0700

    Update GetDataJSON.php

--- GetDataJSON.php
+++ GetDataJSON.php
@@ -34,12 +34,12 @@ class GetData
                 $obj = json_decode($output);
                 $index = self::getIndexProvince($province);
                 if(isset($index)){
-                        $data_province = $obj->Stations[$index]->StationNameTh;
-                        $data_Buil = $obj->Stations[$index]->Observe->Build->Value;
-                        $data_ID = $obj->Stations[$index]->Observe->IDnum->Value;
-                        $data_room = $obj->Stations[$index]->Observe->roomdes->Value;
-                        $data_Devision = $obj->Stations[$index]->Observe->Devisiondes->Value;
-                        $data_tel = $obj->Stations[$index]->Observe->telephone->Value;
+                        $data_province = $obj->;
+                        $data_Buil = $obj->Build->Value;
+                        $data_ID = $obj->IDnum->Value;
+                        $data_room = $obj->roomdes->Value;
+                        $data_Devision = $obj->Devisiondes->Value;
+                        $data_tel = $obj->telephone->Value;
                        
 
                         $data_array = array(
