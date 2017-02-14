71cf75767b61f3c62a372d543b7480281ff4a8f3
beoneben10 <bestthan01@gmail.com>
2017-02-14 08:42:56 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,15 +1,14 @@
 <?php 
-$ch = curl_init(); 
+		$ch = curl_init(); 
                 // set url สำหรับดึงข้อมูล 
                 curl_setopt($ch, CURLOPT_URL, "https://www.namo.xyz/lineben/myfile.json"); 
                 //return the transfer as a string 
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
                 // ตัวแปร $output เก็บข้อมูลทั้งหมดที่ดึงมา 
                 $output = curl_exec($ch); 
-                // ปิดการเชื่อต่อ
-                curl_close($ch);    
+                    
                 // output ออกไปครับ
-                $obj = json_decode($output);
+                $obj = json_decode($output,true);
                 foreach ($obj as $row){
 		echo $row['Buil']." ";
 		echo $row['ID']." ";
@@ -17,3 +16,6 @@ $ch = curl_init();
 		echo $row['Devision']." ";
 		echo $row['tel']." ";
 		echo "<br>";}
+			
+		// ปิดการเชื่อต่อ
+                curl_close($ch);
