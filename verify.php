6711eef93f6a6522488a7fadaffd49eb832662a9
beoneben10 <bestthan01@gmail.com>
2017-02-20 08:02:14 +0700

    Delete verify.php

--- verify.php
+++
@@ -1,8 +0,0 @@
-<?php
-$access_token = 'SmC479aBsZtxrVoqsqEW1KDMT6c/bP6woDbRg2BD56+k/NM86O4XKvG68KVShh4mImg9IQJbE+QaVdGlSUSAbhzuxYN60cRrWdrqa6eyTAf2aOZO2IxluB9A4tAFYjepYUVSX+R1OKfrl9JX2zlc3QdB04t89/1O/w1cDnyilFU=';
-{
-"canvas":{"width" : 700,"height" : 700,"initialScene" : "scenel"},"image":{"image1":{"x" : 0,"y" : 0,"w":700,"h":700}},
-"actions":{"openHomepage":{"type": "web","text":"Open our homepage.","params":{"linkUrl":"http://google.co.th"}},
-"showItem":{"type": "web","text": "Show item.","params":{"linkUrl":"http://google.co.th"}},
-"scenes":{"scene1":{"draw":[{"image": "image1","x" : 0,"y" : 0,"w":700,"h":700}},       
-"listeners":[{"type":"touch","params":[0,0,700,350],"action":"openHomepage"},{"type": "touch","params":[0,350,700,350],"action":"showItem"}}}}}                         
