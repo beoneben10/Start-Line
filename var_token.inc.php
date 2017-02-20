c7cc96921ee5b1766b1df782a0955ef61a7a3a62
beoneben10 <bestthan01@gmail.com>
2017-02-20 08:01:57 +0700

    Delete var_token.inc.php

--- var_token.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$access_token = $_SERVER['SmC479aBsZtxrVoqsqEW1KDMT6c/bP6woDbRg2BD56+k/NM86O4XKvG68KVShh4mImg9IQJbE+QaVdGlSUSAbhzuxYN60cRrWdrqa6eyTAf2aOZO2IxluB9A4tAFYjepYUVSX+R1OKfrl9JX2zlc3QdB04t89/1O/w1cDnyilFU='];
-?>
